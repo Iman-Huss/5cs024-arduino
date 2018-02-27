5230f6d2c61d6621afb9436dc9d276fa12fe27b6
Mohamed Maaroufi <m.maaroufi@wlv.ac.uk>
2018-02-27 13:21:51 +0000

    - WIFI connexion - Send/receive data - Motor control

--- ValveControl.ino
+++ ValveControl.ino
@@ -1,17 +1,40 @@
 #include <SoftwareSerial.h>
+#include <Stepper.h>
+
+#define STEPS 2038
+#define DEBUG true
+
 
 SoftwareSerial ESP8266(10, 11);
 
-String wifiNetwork = "LenovoP2"; // Garder les guillements
+//String wifiNetwork = "MOHAESP"; // Garder les guillements
+//String Password = "password"; // Garder les guillements
+
+String wifiNetwork = "VM563367-2G"; // Garder les guillements
 String Password = "password"; // Garder les guillements
+
+//String wifiNetwork = "LenovoP2"; // Garder les guillements
+//String Password = "password"; // Garder les guillements
+
+Stepper stepper(STEPS, 2, 4, 3, 5);
+
+boolean valveMoving = false;
+
 
 /****************************************************************/
 /*                             INIT                             */
 /****************************************************************/
 void setup()
 {
+  pinMode(9,OUTPUT);
+  digitalWrite(9,LOW);
+  
+  pinMode(12,OUTPUT);
+  digitalWrite(12,LOW);
+
   pinMode(13,OUTPUT);
   digitalWrite(13,LOW);
+  
   Serial.begin(9600);
   
   ESP8266.begin(115200);
@@ -21,25 +44,68 @@ void setup()
   ESP8266.begin(9600);  
   InitESP8266();
   ConnectToWebsite();
+
 }
+
+
+
 /****************************************************************/
 /*                            Loop                              */
 /****************************************************************/
 void loop()
 {
-   while(ESP8266.available())
-   {    
-     Serial.println(ESP8266.readString());
-   }  
-   
+      if (valveMoving == true){
+      digitalWrite(9,HIGH);
+      digitalWrite(13,LOW);
+      digitalWrite(12,LOW);
+      Blink(9);
+    }
+  
+//   while(ESP8266.available())
+//   {    
+//     Serial.println(ESP8266.readString());
+//   }  
+
+  if(ESP8266.available()) // check if the esp is sending a message 
+  {
+    if(ESP8266.find("+IPD,"))
+    {
+      delay(500); // wait for the serial buffer to fill up (read all the serial data)
+      // get the connection id so that we can then disconnect
+      int connectionId = ESP8266.read()-48; // subtract 48 because the read() function returns the ASCII decimal value
+            
+      ESP8266.find("pin="); // advance cursor to "pin="
+      Serial.println("new client"); 
+      int pinNumber = (ESP8266.read()-48); // get first number i.e.
+      Serial.print("new pinNumber : "); 
+      Serial.println(pinNumber, DEC);
+      if(pinNumber == 1)
+      {      digitalWrite(9,HIGH);
+      digitalWrite(13,LOW);
+      digitalWrite(12,LOW);
+        valveMoving = true;
+        openValve();
+      }
+      
+      if(pinNumber == 2)
+      {
+              digitalWrite(9,HIGH);
+      digitalWrite(13,LOW);
+      digitalWrite(12,LOW);
+        valveMoving = true;
+        closeValve();
+      }     
+    }
+  
+  }  
+
 }
+
 
 /* Function to initialise ESP8266 */
 void InitESP8266()
 {  
-  Serial.println("***********************************************************");  
-  Serial.println("********************** INITIALISATION *********************");
-  Serial.println("***********************************************************");  
+  Serial.println("********************** INITIALISATION *********************"); 
   sendToESP8266("AT");
   receiveFromESP8266(2000);
   Serial.println("***********************************************************");
@@ -48,6 +114,7 @@ void InitESP8266()
   Serial.println("***********************************************************");
   sendToESP8266("AT+CWJAP=\""+ wifiNetwork + "\",\"" + Password +"\""); //connect to wifi network
   receiveFromESP8266(10000);
+    
   Serial.println("***********************************************************");
   sendToESP8266("AT+CIFSR"); //Display the IPs adress (client + server)
   receiveFromESP8266(1000);
@@ -59,10 +126,9 @@ void InitESP8266()
   The port number is set for listening.*/
   sendToESP8266("AT+CIPSERVER=1,80");
   receiveFromESP8266(1000);
-  Serial.println("***********************************************************");
   Serial.println("******************* INITIALISATION DONE *******************");
-  Serial.println("***********************************************************");  
 }
+
 
 /* Function to connect to the server */
 void ConnectToWebsite()
@@ -79,7 +145,7 @@ void ConnectToWebsite()
   sendToESP8266(httpreq);
   receiveFromESP8266(10000);
   sendToESP8266("Host: mi-linux.wlv.ac.uk");
-  sendToESP8266("Connection: keep-alive");
+  sendToESP8266("Connection: close");
   sendToESP8266("");   
   receiveFromESP8266(5000);
   Serial.println("\r\n");
@@ -105,6 +171,34 @@ void receiveFromESP8266(const int timeout)
     }
   }
   Serial.print(reponse);   
+}
+
+
+void openValve(){
+  stepper.setSpeed(6); // 6 rpm
+  stepper.step(-2038); // do 2038 steps in the other direction with faster speed -- corresponds to one revolution in 10 seconds
+  valveMoving = false;
+  digitalWrite(13,LOW);
+  digitalWrite(12,HIGH);
+  digitalWrite(9, LOW);
+}
+
+
+void closeValve(){
+  stepper.setSpeed(6); // 6 rpm
+  stepper.step(2038); // do 2038 steps in the other direction with faster speed -- corresponds to one revolution in 10 seconds
+  valveMoving = false;
+  digitalWrite(13,HIGH);
+  digitalWrite(12,LOW);
+  digitalWrite(9, LOW);
+}
+
+
+void Blink(int ledPin) {
+  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(200);                       // wait for a second
+  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+  delay(200);                       // wait for a second
 }
 
 
